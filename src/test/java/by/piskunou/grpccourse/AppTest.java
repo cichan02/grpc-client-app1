9e64849060fa8b2fef047259da6fdd55091b871c
cichan <6684177pt@gmail.com>
2022-10-23 22:54:24 +0300

    init

---
+++ src/test/java/by/piskunou/grpccourse/AppTest.java
@@ -0,0 +1,13 @@
+package by.piskunou.grpccourse;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
